9303d39dd948f987e6c1f8a1242e78a54c690418
Ryan Burns <ryan@grandpad.net>
2020-11-04 09:03:36 -0800

    playing with debug files

--- simple-wof-server/index.js
+++ simple-wof-server/index.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const db = require('./db')
-const scoreRouter = require('./routes/score-router')
+const db = require("./db");
+const scoreRouter = require("./routes/score-router");
 
-const app = express()
-const apiPort = 3000
+const app = express();
+const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/api', scoreRouter)
+app.use("/api", scoreRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
